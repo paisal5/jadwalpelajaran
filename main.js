c8b6785961231b5cdf21a4920a99e79427fb91a6
paisal5 <iyattpaisal@gmail.com>
2024-08-07 10:32:16 +0700

    update

--- main.js
+++ main.js
@@ -23,12 +23,13 @@ const firebaseConfig = {
   measurementId: "G-7Q534CCZNV"
 };
 // Inisialisasi Firebase
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export async function ambilDaftarjadwal() {
+export async function ambilDaftarMapel() {
   const refDokumen = collection(db, "jadwal");
-  const kueri = query(refDokumen, orderBy("id"));
+  const kueri = query(refDokumen, orderBy("hari"));
   const cuplikanKueri = await getDocs(kueri);
 
   let hasil = [];
@@ -37,11 +38,11 @@ export async function ambilDaftarjadwal() {
       id: dok.id,
       hari: dok.data().hari,
       jamke: dok.data().jamke,
-      waktu: dok.data().waktu,
+      waktu: dok.data().wakti,
       mapel: dok.data().mapel,
-      namaguru: dok.data().namaguru,
-      kelas: dok.data().kelas
-     
+      namaguru:dok.data().namaguru,
+      kelas: dok.data().kelas,
+      mp: dok.data().mp,
 
     });
   });
@@ -49,46 +50,4 @@ export async function ambilDaftarjadwal() {
 
 
   return hasil;
-}
-
-export function formatAngka(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
-export async function tambahjadwal(hari, jamke, waktu, mapel, namaguru, kelas) {
-  try {
-    const dokRef = await addDoc(collection(db, 'jadwal'), {
-      hari: hari,
-      jamke: jamke,
-      waktu: waktu,
-      mapel: mapel,
-      namaguru: namaguru,
-      kelas: kelas
-    });
-    console.log('berhasil menembah ' + dokRef.id);
-  } catch (e) {
-    console.log('gagal menambah ' + e);
-  }
-}
-
-export async function hapusjadwal(docId) {
-  await deleteDoc(doc(db, "jadwal", docId));
-}
-
-export async function ubahjadwal(docId, hari, jamke, waktu, mapel, namaguru, kelas) {
-  await updateDoc(doc(db, "jadwal", docId), {
-    hari: hari,
-    jamke: jamke,
-    waktu: waktu,
-    mapel: mapel,
-    namaguru: namaguru,
-    kelas: kelas
-  });
-}
-
-export async function ambiljadwal(docId) {
-  const docRef = await doc(db, "jadwal", docId);
-  const docSnap = await getDoc(docRef);
-
-  return await docSnap.data();
 }
